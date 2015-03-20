f4dcefc19240fbf45759f9556edb07cd7420135e
ikageso <ikamon001@gmail.com>
2015-03-20 22:05:02 +0900

    add testfile

---
+++ PCTime/PCTimeTest/ViewModel/EventLogViewModelTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PCTime.ViewModel;
+using NUnit.Framework;
+using Microsoft.TeamFoundation.MVVM;
+namespace PCTime.ViewModel.Tests
+{
+    [TestFixture()]
+    public class EventLogViewModelTest
+    {
+        [Test()]
+        public void EventLogViewModelTest01()
+        {
+            var vm = new EventLogViewModel();
+
+            var now = DateTime.Now;
+
+            vm.StartDate = now.AddMonths(-1);
+            vm.EndDate = now;
+
+            Assert.True(!(vm as WindowViewModel).HasErrors());
+        }
+
+        [Test()]
+        public void EventLogViewModelTest02()
+        {
+            var vm = new EventLogViewModel();
+
+            var now = DateTime.Now;
+
+            vm.EndDate = now.AddMonths(-1);
+            vm.StartDate = now;
+
+            Assert.True((vm as WindowViewModel).HasErrors());
+        }
+
+        [Test()]
+        public void EventLogViewModelTest03()
+        {
+            var vm = new EventLogViewModel();
+
+            var now = DateTime.Now;
+
+            vm.StartDate = now.AddMonths(-1);
+            vm.EndDate = now.AddDays(1);
+
+            Assert.True((vm as WindowViewModel).HasErrors());
+        }
+
+        [Test()]
+        public void EventLogViewModelTest04()
+        {
+            var vm = new EventLogViewModel();
+
+            var now = DateTime.Now;
+
+            vm.StartDate = now.AddDays(1);
+            vm.EndDate = now.AddDays(1);
+
+            Assert.True((vm as WindowViewModel).HasErrors());
+        }
+
+    }
+}
